9bfeea96de3fa8347b5af56c5f2676b09a2c52d7
William-Ro <william.rodriguez.rocha@est.una.ac.cr>
2022-11-04 17:00:46 -0600

    Won method

---
+++ cmake-build-debug-coverage/CMakeFiles/ProjectSolitaire.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectSolitaire.dir/main.cpp.obj"
+  "CMakeFiles/ProjectSolitaire.dir/main.cpp.obj.d"
+  "ProjectSolitaire.exe"
+  "ProjectSolitaire.exe.manifest"
+  "ProjectSolitaire.pdb"
+  "libProjectSolitaire.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectSolitaire.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
